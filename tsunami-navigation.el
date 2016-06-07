7253484a961e4ecb462d76de1c57a19389ac2c04
Andy Moreland <amoreland@palantir.com>
2016-06-07 14:54:44 -0700

    Fix tide-references to work anywhere in the line

--- tsunami-navigation.el
+++ tsunami-navigation.el
@@ -20,8 +20,24 @@
   (interactive "P")
   (lexical-let ((arg arg))
     (tsunami--get-quickinfo-at-point (lambda (response)
-                                       (if (or arg (string-equal "alias" (tide-plist-get response :body :kind)))
+                                       (if (or arg
+                                               (string-equal "alias" (tide-plist-get response :body :kind)))
                                            (tide-command:type-definition 'tsunami--jump-to-definition-and-retry)
                                          (tide-command:definition 'tsunami--jump-to-definition-and-retry))))))
+
+(defun tsunami-references ()
+  (interactive)
+  (let ((reference-window (tide-references)))
+    (when reference-window
+      (select-window reference-window))))
+
+
+(defun tsunami--find-tide-reference-on-line ()
+  (unless (get-text-property (point) 'tide-reference)
+    (beginning-of-line)
+    (goto-char (next-single-property-change (point) 'tide-reference))))
+
+;; Jump to the reference on the current line before `goto`ing it.
+(advice-add 'tide-goto-reference :before #'tsunami--find-tide-reference-on-line)
 
 (provide 'tsunami-navigation)
