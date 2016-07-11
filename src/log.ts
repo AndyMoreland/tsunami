9ec159d9ecbdd26a38ef7bfe33cec8e35cb75331
Andy Moreland <amoreland@palantir.com>
2016-07-11 14:13:52 -0700

    Log with PID

--- src/log.ts
+++ src/log.ts
@@ -1,13 +1,13 @@
 import * as fs from "fs";
 
 export default function log(...args: any[]): void {
-    fs.appendFile("/Users/amoreland/tsunami/log.txt", "\n\n" + args.join(", "));
+    fs.appendFile("/Users/amoreland/tsunami/log.txt", "\n\n" + "[" + process.pid + "]: " + args.join(", "));
 }
 
 export function logWithCallback(cb: Function, ...args: any[]): void {
-    fs.appendFile("/Users/amoreland/tsunami/log.txt", "\n\n" + args.join(", "), cb);
+    fs.appendFile("/Users/amoreland/tsunami/log.txt", "\n\n" + "[" + process.pid + "]: " + args.join(", "), cb);
 }
 
 export function logSync(...args: any[]): void {
-    fs.appendFileSync("/Users/amoreland/tsunami/log.txt", "\n\n" + args.join(", "));
+    fs.appendFileSync("/Users/amoreland/tsunami/log.txt", "\n\n" + "[" + process.pid + "]: " + args.join(", "));
 }
