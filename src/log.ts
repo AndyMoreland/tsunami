9594636618ee714361c413823d46b7a2623350dc
Andy Moreland <amoreland@palantir.com>
2016-10-04 10:46:49 -0700

    Flag off logging

--- src/log.ts
+++ src/log.ts
@@ -1,13 +1,23 @@
 import * as fs from "fs";
 
+const LOGGING_ENABLED = false;
+
 export default function log(...args: any[]): void {
-    fs.appendFile("/Users/amoreland/tsunami/log.txt", "\n\n" + "[" + process.pid + "]: " + args.join(", "));
+    if (LOGGING_ENABLED) {
+        fs.appendFile("/Users/amoreland/tsunami/log.txt", "\n\n" + "[" + process.pid + "]: " + args.join(", "));
+    }
 }
 
 export function logWithCallback(cb: Function, ...args: any[]): void {
-    fs.appendFile("/Users/amoreland/tsunami/log.txt", "\n\n" + "[" + process.pid + "]: " + args.join(", "), cb);
+    if (LOGGING_ENABLED) {
+        fs.appendFile("/Users/amoreland/tsunami/log.txt", "\n\n" + "[" + process.pid + "]: " + args.join(", "), cb);
+    } else {
+        cb();
+    }
 }
 
 export function logSync(...args: any[]): void {
-    fs.appendFileSync("/Users/amoreland/tsunami/log.txt", "\n\n" + "[" + process.pid + "]: " + args.join(", "));
+    if (LOGGING_ENABLED) {
+        fs.appendFileSync("/Users/amoreland/tsunami/log.txt", "\n\n" + "[" + process.pid + "]: " + args.join(", "));
+    }
 }
