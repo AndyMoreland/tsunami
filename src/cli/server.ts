98ee3336848d6f3e02e027519f96620a415885f1
Andy Moreland <amoreland@palantir.com>
2016-10-04 21:12:20 -0700

    Move index -> cli/server

--- src/cli/server.ts
+++ src/cli/server.ts
@@ -1,16 +1,15 @@
-/// <reference path="../typings/node/node.d.ts" />
-import { Tsunami } from "./Tsunami";
-import { ChangeCommandDefinition } from "./commands/ChangeCommand";
-import { FetchSymbolLocationsDefinition } from "./commands/FetchSymbolLocations";
-import { GetContainingExpressionsDefinition } from "./commands/GetContainingExpressions";
-import { GetContainingScopesDefinition } from "./commands/GetContainingScopes";
-import { GetPropertiesOfSymbolDefinition } from "./commands/GetPropertiesOfSymbol";
-import { MoveSymbolCommandDefinition } from "./commands/MoveSymbolCommand";
-import { OrganizeImportsCommandDefinition } from "./commands/OrganizeImportsCommand";
-import { ReloadCommandDefinition } from "./commands/ReloadCommand";
-import { SaveToCommandDefinition } from "./commands/SaveToCommand";
-import log, { logWithCallback } from "./log";
-import { TsProject } from "./tsProject";
+import { Tsunami } from "../Tsunami";
+import { ChangeCommandDefinition } from "../commands/ChangeCommand";
+import { FetchSymbolLocationsDefinition } from "../commands/FetchSymbolLocations";
+import { GetContainingExpressionsDefinition } from "../commands/GetContainingExpressions";
+import { GetContainingScopesDefinition } from "../commands/GetContainingScopes";
+import { GetPropertiesOfSymbolDefinition } from "../commands/GetPropertiesOfSymbol";
+import { MoveSymbolCommandDefinition } from "../commands/MoveSymbolCommand";
+import { OrganizeImportsCommandDefinition } from "../commands/OrganizeImportsCommand";
+import { ReloadCommandDefinition } from "../commands/ReloadCommand";
+import { SaveToCommandDefinition } from "../commands/SaveToCommand";
+import log, { logWithCallback } from "../log";
+import { TsProject } from "../tsProject";
 
 /* HACK */
 process.on("uncaughtException", (err: any) => {
